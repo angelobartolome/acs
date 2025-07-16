8ff3b5b9ac36948b0a1c6f01d64ad585b017b683
Angelo Bartolome <angelo.m.bartolome@gmail.com>
2025-07-16 18:33:01 -0300

    refactor: change parameter types for add_point and add_line methods to references

--- src/bindings/solver.rs
+++ src/bindings/solver.rs
@@ -17,12 +17,12 @@ impl WrappedConstraintSolver {
         }
     }
 
-    pub fn add_point(&mut self, point: crate::Point) -> usize {
-        self.inner.add_point(point)
+    pub fn add_point(&mut self, point: &crate::Point) -> usize {
+        self.inner.add_point(*point)
     }
 
-    pub fn add_line(&mut self, line: crate::Line) -> usize {
-        self.inner.add_line(line)
+    pub fn add_line(&mut self, line: &crate::Line) -> usize {
+        self.inner.add_line(*line)
     }
 
     pub fn add_vertical_constraint(&mut self, line_id: usize) -> Result<(), String> {
@@ -41,5 +41,8 @@ impl WrappedConstraintSolver {
         self.inner.get_state_as_string()
     }
 
+    pub fn get_point(&self, id: usize) -> Option<crate::Point> {
+        self.inner.get_point(id).cloned()
+    }
     // Add more methods as needed
 }
